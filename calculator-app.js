5f1281f3fa5614cb5cb0d9ef9ba0e5fe3be0ae70
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-09-29 17:01:05 +0100

    Saving current work and notes

--- calculator-app.js
+++ calculator-app.js
@@ -1,3 +1,7 @@
+//Regular global variables:
+
+const history = [0, 0];
+
 //Object selectors
 
 const calcMain  = document.querySelector(".calculator__main");
@@ -7,13 +11,46 @@ const calcIO    = document.querySelector(".calculator__screen");
 //Selector callback functions
 
 const mainCallback = (event) => {
-    //test code:
-    event.preventDefault()
-    console.log(event.target.elements[0].value);    //the submit event barfs out everything it knows:
+    //test code- we'll igrate functions into their own niches when needed:
+    event.preventDefault() //prevent page refresh on submbit.
+
+    const target = event.target.elements[0];
+    //the submit event barfs out everything it knows:
     // so grab the first element from that list, pray it's the input (We might have to fix it if it's not)...
     //... and call value on it.
+    
+    console.log(target.value);
+
+    //Logic:
+    //On a submit, we want to write the target value to a history buffer
+    //Then perform the right operaton on the two most recent history items:
+    ///finally push back to "Screen"
+
+    history.push((target.value) /1) // Div by 1 forces int conv.
+    
+    console.log(history.slice(-2));
+    target.value = multiplication(history.slice(-2));
+
+    //Doesn't work, We'll need to refactor.
+    //Thinking making history track args+operation instead.
 }
 
+
+const addition = (range) => {
+    return range.reduce((num, total) => total += num);
+}
+
+const subtraction = (range) => {
+    return range.reduce((num, total) => total -= num);
+}
+
+const multiplication = (range) => {
+    return range.reduce((num, total) => total = total * num)
+}
+
+const clearAll = () => {
+    history.push(0);
+}
 
 //Callback execution:
 
