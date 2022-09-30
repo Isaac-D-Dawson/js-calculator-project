8075481b50d7b22eb2f7ec02c1f0510bd3584dd8
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-09-30 11:34:34 +0100

    Inegrated basic read/run/return functionality.

--- calculator-app.js
+++ calculator-app.js
@@ -1,6 +1,5 @@
 //Regular global variables:
 
-const history = [0, 0];
 
 //Object selectors
 
@@ -9,32 +8,6 @@ const calcIO    = document.querySelector(".calculator__screen");
 
 
 //Selector callback functions
-
-const mainCallback = (event) => {
-    //test code- we'll igrate functions into their own niches when needed:
-    event.preventDefault() //prevent page refresh on submbit.
-
-    const target = event.target.elements[0];
-    //the submit event barfs out everything it knows:
-    // so grab the first element from that list, pray it's the input (We might have to fix it if it's not)...
-    //... and call value on it.
-    
-    console.log(target.value);
-
-    //Logic:
-    //On a submit, we want to write the target value to a history buffer
-    //Then perform the right operaton on the two most recent history items:
-    ///finally push back to "Screen"
-
-    history.push((target.value) /1) // Div by 1 forces int conv.
-    
-    console.log(history.slice(-2));
-    target.value = multiplication(history.slice(-2));
-
-    //Doesn't work, We'll need to refactor.
-    //Thinking making history track args+operation instead.
-}
-
 
 const addition = (range) => {
     return range.reduce((num, total) => total += num);
@@ -50,6 +23,52 @@ const multiplication = (range) => {
 
 const clearAll = () => {
     history.push(0);
+}
+
+//History buffer contains the last two used values(in order), the operator invoked on them, and the result of the operation.
+
+const history = [
+    {
+        values      : [0, 0],
+        operator    : addition,
+        result      : 0
+    }
+];
+
+
+const mainCallback = (event) => {
+    //test code- we'll igrate functions into their own niches when needed:
+    event.preventDefault() //prevent page refresh on submbit.
+
+    const targetScreen = event.target.elements[0];
+    //the submit event barfs out everything it knows:
+    // so grab the first element from that list, pray it's the input (We might have to fix it if it's not)...
+    //... and call value on it.
+    
+    console.log(targetScreen.value);
+
+
+    currentOperator = addition  //Later will be dtermined by form data.
+
+    //Logic:
+    //On a submit, we want to write the target value to a history buffer
+    //Then perform the right operaton on the two most recent history items:
+    ///finally push back to "Screen"
+
+    history.push({
+        values      : [history.slice(-1)[0].result, targetScreen.value/1],    //puses current value and last result into history stack (Div by 1 to force to int).
+        operator    : currentOperator,
+        result      : currentOperator([history.slice(-1)[0].result, targetScreen.value/1])
+    })
+
+    console.log(history);
+
+    //Doesn't work, We'll need to refactor.
+    //Thinking making history track args+operation instead.
+
+    // IDEAS:
+    //-history uses callbackfunctions to determine how it displays.
+    //Two input boxes, the second of which gets the output.
 }
 
 //Callback execution:
