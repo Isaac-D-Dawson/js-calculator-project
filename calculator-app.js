cd63c019f161a253ff64a442f89a4f9cd6bf6fee
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-09-30 12:34:14 +0100

    Core functionality implimented

--- calculator-app.js
+++ calculator-app.js
@@ -1,29 +1,51 @@
 //Regular global variables:
 
 
+
 //Object selectors
 
-const calcMain  = document.querySelector(".calculator__main");
-const calcIO    = document.querySelector(".calculator__screen");
+const calcMain      = document.querySelector(".calculator__main");
+const calcIO        = document.querySelector(".calculator__screen");
 
+const calcButtons   = document.querySelectorAll(".calculator__button"); //All calc buttons for quick-disable.
+const calcAdd       = document.querySelector(".calculator__button--add");
+const calcSub       = document.querySelector(".calculator__button--sub");
+const calcMul       = document.querySelector(".calculator__button--mul");
+const calcDiv       = document.querySelector(".calculator__button--div");
+const calcEql       = document.querySelector(".calculator__button--eql");
+const calcClr       = document.querySelector(".calculator__button--clr");
 
 //Selector callback functions
 
 const addition = (range) => {
-    return range.reduce((num, total) => total += num);
+    return range.reduce((total, num) => total += num);
 }
 
 const subtraction = (range) => {
-    return range.reduce((num, total) => total -= num);
+    return range.reduce((total, num) => total -= num);
 }
 
 const multiplication = (range) => {
-    return range.reduce((num, total) => total = total * num)
+    return range.reduce((total, num) => total = total * num)
+}
+
+const division = (range) => {
+    return range.reduce((num, divisor) => num = num / divisor)
+}
+
+const equal = () => { 
+    targetScreen = history.slice(-1)[0].result;
 }
 
 const clearAll = () => {
-    history.push(0);
+    history.push({
+                values  : [0, 0],
+            operator    : addition,
+            result      : 0
+        }
+    );
 }
+
 
 //History buffer contains the last two used values(in order), the operator invoked on them, and the result of the operation.
 
@@ -36,6 +58,9 @@ const history = [
 ];
 
 
+let currentOperator = addition //Default setting.
+
+
 const mainCallback = (event) => {
     //test code- we'll igrate functions into their own niches when needed:
     event.preventDefault() //prevent page refresh on submbit.
@@ -45,10 +70,7 @@ const mainCallback = (event) => {
     // so grab the first element from that list, pray it's the input (We might have to fix it if it's not)...
     //... and call value on it.
     
-    console.log(targetScreen.value);
-
-
-    currentOperator = addition  //Later will be dtermined by form data.
+    console.log(targetScreen.value);  //Later will be dtermined by form data.
 
     //Logic:
     //On a submit, we want to write the target value to a history buffer
@@ -71,6 +93,31 @@ const mainCallback = (event) => {
     //Two input boxes, the second of which gets the output.
 }
 
+//Create callback that sets all buttons to inactive, and target state to active:
+const setButtonActive = (event) => {
+    calcButtons.forEach((button) => {
+        button.classList.remove("active");
+    })
+    event.target.classList.add("active");
+    //Add in functionality to set operator from this
+    console.log(`current operator is ${currentOperator}`);
+    event.target.classList.forEach((itemClass) => {
+        const validOperators = {"add" : addition,
+                                "sub" : subtraction,
+                                "mul" : multiplication,
+                                "div" : division,
+                                "eql" : equal,
+                                "clr" : clearAll
+                            }
+        currentOperator = (validOperators[itemClass.substring(20)] ?? currentOperator )
+    })//console.log(itemClass.substring(20)));
+    console.log(`new operator is ${currentOperator}`);
+}
+
+
 //Callback execution:
 
 calcMain.addEventListener("submit", mainCallback);
+calcButtons.forEach((button) => {
+    button.addEventListener("click", setButtonActive);
+})
